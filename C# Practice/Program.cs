c2895028ec8a22567d8ace42a3a0999323a84a2f
memoriesbytalha <muhammadtalha7893@yahoo.com>
2025-01-27 12:44:10 +0500

    First Commit

---
+++ C# Practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Niggac!");
+            //Console.WriteLine(3 + 3);   
+            string name = "Talha Ali Panda";
+            //int num = 14;
+            //Console.WriteLine("{0} {1}",Name,num);
+            //const int pi = 3;
+            //Console.WriteLine($"{pi} {Name} {num}");
+
+
+            //int myInt = 9;
+            //double myDouble = myInt;       // Automatic casting: int to double
+
+            //Console.WriteLine(myInt);      // Outputs 9
+            //Console.WriteLine(myDouble);
+
+
+            //Console.WriteLine("Writre your name");
+            //string username = Console.ReadLine();
+            //Console.WriteLine("Hello " + username);
+            //int x = 6;
+            //Console.WriteLine(x|=3);
+            //int x = 5;
+            //int y = 3;
+            //Console.WriteLine(x < y); // returns True because 5 is greater than 3
+            //Console.WriteLine(name.ToLower());
+
+            //string last_name = name.Substring(0, 4);
+            //Console.WriteLine(last_name);
+        }
+    }                                          
+}                                              
